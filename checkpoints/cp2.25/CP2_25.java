9d8de34dfed4e76068bc0a2325ddd6d7bf098042
devjoco <devonjoconnor@gmail.com>
2020-10-15 14:03:26 -0400

    Add .gitignore, cp2.25, cp2.26, hello

---
+++ checkpoints/cp2.25/CP2_25.java
@@ -0,0 +1,7 @@
+public class CP2_25 {
+    public static void main(String[] args){
+        short totalPay, basePay = 500, bonus = 1000;
+        totalPay = (short) (basePay + bonus);
+        System.out.println(totalPay);
+    }
+}
---
+++ checkpoints/cp2.26/CP2_26.java
@@ -0,0 +1,9 @@
+public class CP2_26 {
+    public static void main(String[] args){
+        float a = 1.2F;
+        double b = 4.8;
+        a = (float) b;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
