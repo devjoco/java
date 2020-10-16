1b78e4b905061fa046ed1b7d61769d73c42652cf
devjoco <devonjoconnor@gmail.com>
2020-10-16 19:15:16 -0400

    Remove old-style named files

--- checkpoints/cp2.26/CP2_26.java
+++
@@ -1,9 +0,0 @@
-public class CP2_26 {
-    public static void main(String[] args){
-        float a = 1.2F;
-        double b = 4.8;
-        a = (float) b;
-        System.out.println(a);
-        System.out.println(b);
-    }
-}
