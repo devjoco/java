b62931bb26a338f80762af6934626d8f3c371198
devjoco <devonjoconnor@gmail.com>
2020-10-20 13:39:03 -0400

    Add ranges for factory stats, refine output

--- ch3/factory/DemoFactory.java
+++ ch3/factory/DemoFactory.java
@@ -1,37 +1,36 @@
 public class DemoFactory {
     public static void main(String[] args) {
-        final int WTM = 1000;
-        int wph, spd, hps, days;
+        final int NUM_FACTORIES = 4;
+        final int[] wphRange = {5, 10, 15, 20};
+        final int[] spdRange = {1, 2, 3, 4};
+        final int[] hpsRange = {4, 6, 8, 10};
         String shiftPlur, dayPlur;
+        int wph, spd, hps, days;
+        int wtm = args.length == 1? Integer.parseInt(args[0]) : 1000;
 
-        Factory slowFac = new Factory(5, 1, 6);
-        Factory avgFac  = new Factory(); // Default with 10 WPH, 2 SPD, 8 HPS
-        Factory fastFac = new Factory(15, 2, 8);
-        Factory maxFac  = new Factory(15, 3, 8);
+        Factory[] facs = new Factory[NUM_FACTORIES];
+        for(int i=0; i<NUM_FACTORIES; i+=1) 
+            facs[i] = new Factory(wphRange[i], spdRange[i], hpsRange[i], wtm);
 
-        Factory[] facs = new Factory[4];
-        facs[0] = slowFac;
-        facs[1] = avgFac;
-        facs[2] = fastFac;
-        facs[3] = maxFac;
-
-        System.out.printf("To make %d widgets it'll take:\n", WTM);
+        /* Output:
+         * To make {wtm} widgets it'll take:
+         *     - {m1} days w/ {y1} {z1}-hour shifts at {x1} widgets/hour. 
+         *     - {m2} days w/ {y2} {z2}-hour shifts at {x2} widgets/hour. 
+         *        …            …    …                   … 
+         *     - {mn} days w/ {yn} {zn}-hour shifts at {xn} widgets/hour. 
+         */
+        System.out.printf("To make %,d widgets it'll take:\n", wtm);
         for(int i=0; i<facs.length; i+=1) {
-            facs[i].setWTM(WTM);
+            facs[i].setWTM(wtm);
             wph       = facs[i].getWPH();
             spd       = facs[i].getSPD();
             hps       = facs[i].getHPS();
             days      = (int)Math.ceil(facs[i].getDTM());
-            shiftPlur = spd == 1  ? "" : "s";
+            shiftPlur = spd == 1  ? " " : "s";
             dayPlur   = days == 1 ? "" : "s";
 
-            /*
-             * End Result:
-             * To make 1000 widgets it'll take:
-             *     - 5 days with 2 8-hour shifts making 10 widgets per hour. 
-             */
             System.out.printf(
-                    "\t%3d day%s w/ %d %d-hour shift%s at %2d widgets/hour\n",
+                    "%,7d day%s w/ %d %2d-hour shift%s at %2d widgets/hour\n",
                     days, dayPlur, spd, hps, shiftPlur, wph
             );
         }
--- ch3/factory/Factory.java
+++ ch3/factory/Factory.java
@@ -1,12 +1,30 @@
+/**
+ * Widget Factory -- Prog Challenge 6, p179
+ * A Factory:
+ *  - Creates x widgets per hour  (default of 10 wph)
+ *  - Has     y shifts  per day   (default of 2 spd) 
+ *  - Has     z hours   per shift (default of 8 hps)
+ * 
+ * There is no limit to the number of widgets that can be produced in an hour
+ *
+ * Shifts cannot be longer than a day, so if specified hps is more than 24,
+ * then it is shortened to 24hrs
+ *
+ * Shifts cannot run over onto another day, so if spd * hps is more than 24,
+ * then spd will be set to as many as possible without running over
+ *
+ * widgetsToMake is 0 to start, but can be changed with setWTM()
+ */
 public class Factory {
-    private int widgetsPerHour;
-    private int shiftsPerDay;
-    private int hoursPerShift;
-    private int widgetsToMake;
+    private final int DAY_HRS = 24;
+    private int widgetsPerHour; // No limit
+    private int shiftsPerDay;   // Can't span multiple days
+    private int hoursPerShift;  // Can't be more than 24
+    private int widgetsToMake; 
 
     /**
-     * A no-arg constructor that creates a factory with a default production
-     * capacity, specified by Widget Factory Programming Challenge on p.179
+     * No-arg constructor, creates factory w/ default production capacity, and
+     * no widgets to be made yet
      */
     public Factory() {
         widgetsPerHour = 10;
@@ -16,13 +34,37 @@ public class Factory {
     }
 
     /**
-     * A constructor to create a factory with customized production capacity.
+     * Creates default factory and sets the number of widgets to make
+     */
+    public Factory(int wtm) {
+        widgetsPerHour = 10;
+        shiftsPerDay   = 2;
+        hoursPerShift  = 8;
+        widgetsToMake  = wtm;
+    }
+
+    /**
+     * A constructor to create a factory with customized production capacity, 
+     * and no widgets to be made yet
      */
     public Factory(int wph, int spd, int hps) {
         widgetsPerHour = wph;
-        shiftsPerDay   = spd;
-        hoursPerShift  = (hps > 24)? 24 : hps;
+        hoursPerShift  = (hps > DAY_HRS)? DAY_HRS : hps;
+        shiftsPerDay   = (hoursPerShift * spd > 24)? DAY_HRS / hoursPerShift :
+                                                     spd;
         widgetsToMake  = 0;
+    }
+
+    /**
+     * A constructor to create a factory with customized production capacity, 
+     * and sets the number of widgets to make
+     */
+    public Factory(int wph, int spd, int hps, int wtm) {
+        widgetsPerHour = wph;
+        hoursPerShift  = (hps > DAY_HRS)? DAY_HRS : hps;
+        shiftsPerDay   = (hoursPerShift * spd > 24)? DAY_HRS / hoursPerShift :
+                                                     spd;
+        widgetsToMake  = wtm;
     }
     
     // Setters
