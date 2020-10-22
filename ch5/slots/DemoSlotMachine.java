9268e5b0b29683a47fbd5947e919c6fc7927651a
devjoco <devonjoconnor@gmail.com>
2020-10-22 00:08:43 -0400

    Add skeleton for SlotMachine class

---
+++ ch5/slots/DemoSlotMachine.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class DemoSlotMachine {
+    public static void main(String[] args) {
+        SlotMachine machine = new SlotMachine();
+    }
+}
---
+++ ch5/slots/SlotMachine.java
@@ -0,0 +1,15 @@
+/**
+ * Slot Machine Simulation -- Programming Challenge 21 p332
+ *
+ *   - balance: double (Money deposited into the machine)
+ *   - items: An array of possible words that can pop up on machine
+ *
+ *   + SlotMachine(): void (Creates a slotmachine with a $0 blanace)
+ *   + SlotMachine(deposit: double): void (SlotMachine with an initial balance)
+ *   + spin(wager: double): void (Simulates a spin)
+ *   + deposit(amount: double): void (Adds money to the machine's balance)
+ *   + withdraw(amount: double): void (Removes money from machine's balance)
+ *   + getBalance(): double (Returns amount of money in machine)
+ */
+public class SlotMachine {
+}
