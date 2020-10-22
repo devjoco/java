2b3b98b05797e62c020bc9e2f7cc365826a5e462
devjoco <devonjoconnor@gmail.com>
2020-10-21 23:09:11 -0400

    Fix imports in Demo

--- ch5/uppercasefile/DemoUpperCaseFile.java
+++ ch5/uppercasefile/DemoUpperCaseFile.java
@@ -1,3 +1,5 @@
+import java.io.*;
+
 public class DemoUpperCaseFile {
     public static void main(String[] args) throws IOException {
         UpperCaseFile ucf = new UpperCaseFile("myLowerCaseFile.txt", 
