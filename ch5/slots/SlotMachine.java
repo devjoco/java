bd5a0e4f47d962127c226b9d84cd2c11ec3b2c38
devjoco <devonjoconnor@gmail.com>
2020-10-22 10:21:50 -0400

    Add fields for SlotMachine

--- ch5/slots/SlotMachine.java
+++ ch5/slots/SlotMachine.java
@@ -4,12 +4,16 @@
  *   - balance: double (Money deposited into the machine)
  *   - items: An array of possible words that can pop up on machine
  *
- *   + SlotMachine(): void (Creates a slotmachine with a $0 blanace)
- *   + SlotMachine(deposit: double): void (SlotMachine with an initial balance)
- *   + spin(wager: double): void (Simulates a spin)
- *   + deposit(amount: double): void (Adds money to the machine's balance)
- *   + withdraw(amount: double): void (Removes money from machine's balance)
- *   + getBalance(): double (Returns amount of money in machine)
+ *   + SlotMachine()           : void   (Creates a slotmachine w/ $0 blanace)
+ *   + SlotMachine(amt: double): void   (SlotMachine with an initial balance)
+ *   + spin(wager: double)     : void   (Simulates a spin)
+ *   + deposit(amount: double) : void   (Adds money to the machine's balance)
+ *   + withdraw(amount: double): void   (Removes money from machine's balance)
+ *   + getBalance()            : double (Returns amount of money in machine)
  */
 public class SlotMachine {
+    final private String[] items = {"Cherries", "Oranges", "Plums",
+                                    "Bells",    "Melons",  "Bars"};
+    private double balance;
+
 }
