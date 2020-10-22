5354d28757ebfcc66cb773512794d8b009f271dc
devjoco <devonjoconnor@gmail.com>
2020-10-22 11:37:59 -0400

    Demo deposits init amt in machine, gets user choice

--- ch5/slots/DemoSlotMachine.java
+++ ch5/slots/DemoSlotMachine.java
@@ -2,6 +2,26 @@ import java.util.Scanner;
 
 public class DemoSlotMachine {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         SlotMachine machine = new SlotMachine();
+        double totalDeposited, totalWinnings;
+        char choice;
+        
+        System.out.println("How much money will you be depositing?");
+        totalDeposited = Double.parseDouble(scan.nextLine());
+        machine.deposit(totalDeposited);
+
+        while(machine.getBalance() > 0.00) {
+            // User can spin, deposit, withdraw, end game
+            System.out.println("What do you want to do:");
+            System.out.println("(S) Spin");
+            System.out.println("(D) Deposit Money");
+            System.out.println("(W) Withdraw Money");
+            System.out.println("(E) End Game");
+            choice = scan.nextLine().toLowerCase().charAt(0);
+            
+        }
+
+
     }
 }
