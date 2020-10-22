533c30e58078361eef75fbe62803c2e9bea64126
devjoco <devonjoconnor@gmail.com>
2020-10-21 23:08:17 -0400

    Add Demo for UpperCaseFile

---
+++ ch5/uppercasefile/DemoUpperCaseFile.java
@@ -0,0 +1,6 @@
+public class DemoUpperCaseFile {
+    public static void main(String[] args) throws IOException {
+        UpperCaseFile ucf = new UpperCaseFile("myLowerCaseFile.txt", 
+                                              "myOtherFile.txt");
+    }
+}
