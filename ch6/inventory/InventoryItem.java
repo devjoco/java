01cee622ec3a47175af8a7fbba2932c50867f62b
devjoco <devonjoconnor@gmail.com>
2020-10-27 12:40:56 -0400

    Add toString method, test all constructors

--- ch6/inventory/InventoryItem.java
+++ ch6/inventory/InventoryItem.java
@@ -35,8 +35,19 @@ public class InventoryItem {
         return units;
     }
 
+    public String toString() {
+        return "{("+units+") "+description+"}";
+    }
+
     public static void main(String[] args) {
         InventoryItem i1 = new InventoryItem();
+        InventoryItem i2 = new InventoryItem("Rake");
+        InventoryItem i3 = new InventoryItem("Shovel", 10);
+        InventoryItem i4 = new InventoryItem(i3);
+
         System.out.println(i1);
+        System.out.println(i2);
+        System.out.println(i3);
+        System.out.println(i4);
     }
 }
