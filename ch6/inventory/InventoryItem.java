c8b83475b3e0132809a5d1cf35d188bf135f8f20
devjoco <devonjoconnor@gmail.com>
2020-10-27 12:34:17 -0400

    Add InventoryItem class

---
+++ ch6/inventory/InventoryItem.java
@@ -0,0 +1,37 @@
+public class InventoryItem {
+    private String description;
+    private int units;
+
+    public InventoryItem() {
+        this("", 0);
+    }
+    
+    public InventoryItem(String d) {
+        this(d, 0);
+    }
+    
+    public InventoryItem(InventoryItem i) {
+        this(i.description, i.units);
+    }
+
+    public InventoryItem(String d, int u) {
+        description = d;
+        units = u;
+    }
+
+    public void setDescription(String d) {
+        description = d;
+    }
+
+    public void setUnits(int u) {
+        units = u;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getUnits() {
+        return units;
+    }
+}
