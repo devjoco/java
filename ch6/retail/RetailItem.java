6bb372629fdce565775dc95bb8fa51b8b4b2e623
devjoco <devonjoconnor@gmail.com>
2020-10-27 23:13:53 -0400

    Add RetailItem constructors

--- ch6/retail/RetailItem.java
+++ ch6/retail/RetailItem.java
@@ -4,12 +4,28 @@ public class RetailItem {
     private String description;
     private int itemNumber;
     private CostData cost;
+    private static double WHOLESALE_PERCENTAGE;
 
     public RetailItem(String desc, int itemNum, 
             double wholesale, double retail) {
         description = desc;
         itemNumber = itemNum;
         cost = new CostData(wholesale, retail);
+    }
+    public RetailItem(String desc, double wholesale, double retail) {
+        this(desc, -1, wholesale, retail);
+    }
+    public RetailItem(double wholesale, double retail) {
+        this("N/A", -1, wholesale, retail);
+    }
+    public RetailItem(double retail) {
+        this("N/A", -1, retail * WHOLESALE_PERCENTAGE, retail);
+    }
+    public RetailItem(String desc, double retail) {
+        this(desc, -1, retail * WHOLESALE_PERCENTAGE, retail);
+    }
+    public RetailItem(String desc, int itemNum, double retail) {
+        this(desc, itemNum, retail * WHOLESALE_PERCENTAGE, retail);
     }
 
     public double getRetail()    { return cost.retail; }
