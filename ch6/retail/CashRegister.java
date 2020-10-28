bb6ebb113e59284b35f75382c702506254991268
devjoco <devonjoconnor@gmail.com>
2020-10-27 22:42:59 -0400

    Finish CashRegister methods

--- ch6/retail/CashRegister.java
+++ ch6/retail/CashRegister.java
@@ -1,9 +1,23 @@
 public class CashRegister {
+    private RetailItem item;
+    private int quantity;
+    private double TAX = 0.06;
+
     public CashRegister(RetailItem item, int quantity) {
+        this.item = item;
+        this.quantity = quantity;
     }
 
-    public double getSubtotal() {}
-    public double getTax() {}
-    public double getTotal() {}
+    public double getSubtotal() {
+        return item.getRetail() * quantity;
+    }
+
+    public double getTax() {
+        return getSubtotal() * TAX;
+    }
+
+    public double getTotal() {
+        return getSubtotal() + getTax();
+    }
     
 }
