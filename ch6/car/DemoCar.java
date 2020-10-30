f8fa8170d5c7093cef44728c8ccbc54669a1a35c
devjoco <devonjoconnor@gmail.com>
2020-10-30 16:40:42 -0400

    Add skeleton and docstring for DemoCar

---
+++ ch6/car/DemoCar.java
@@ -0,0 +1,14 @@
+/**
+ * Simulates a car driving until running out of gas.
+ *
+ * Responsibilities:
+ *   - Create instance of FuelGauge and Odometer
+ *   - Have user determine how much gas to put in, up to 15 Gallons
+ *   - Simulate driving car by incrementing Odometer until fuel runs out
+ *   - Print car mileage and fuel amount during each iteration of loop
+ */
+public class DemoCar {
+    public static void main(String[] args) {
+
+    }
+}
