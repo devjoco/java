e76dcf038e00213a14234049ff03e29d8aa8dde7
devjoco <devonjoconnor@gmail.com>
2020-10-30 15:37:52 -0400

    Add more in-depth error messages

--- ch6/geometry/Geometry.java
+++ ch6/geometry/Geometry.java
@@ -18,9 +18,13 @@ public class Geometry {
 
     public static double rectangleArea(double l, double w) {
         double ans = -1.0;
-        if(l < 0.0) {
+        boolean negL = l < 0.0;
+        boolean negW = w < 0.0;
+        if(negL && negW) {
+            System.err.println("Length and width must be positive!");
+        } else if(negL) {
             System.err.println("Length must be positive!");
-        } else if(w < 0.0) {
+        } else if(negW) {
             System.err.println("Width must be positive!");
         } else {
             ans = l * w;
@@ -30,9 +34,13 @@ public class Geometry {
 
     public static double triangleArea(double b, double h) {
         double ans = -1.0;
-        if(b < 0.0) {
+        boolean negB = b < 0.0;
+        boolean negH = h < 0.0;
+        if(negB && negH) {
+            System.err.println("Base and height must be positive!");
+        } else if(negB) {
             System.err.println("Base must be positive!");
-        } else if(h < 0.0) {
+        } else if(negH) {
             System.err.println("Height must be positive!");
         } else {
             ans = 0.5 * b * h;
