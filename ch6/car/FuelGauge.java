fd22438c795c048ecfe1929eccb57815b2941960
devjoco <devonjoconnor@gmail.com>
2020-10-30 16:27:52 -0400

    Add docstrings to FuelGauge/Odometer classes

---
+++ ch6/car/FuelGauge.java
@@ -0,0 +1,10 @@
+/**
+ * Simulates a fuel gauge.
+ *
+ * Responsibilities:
+ *   - Know/report car's current fuel in gallons
+ *   - Increment fuel by 1 gallon (Max of 15).
+ *   - Decrement fuel by 1 gallon (Min of 0).
+ */
+public class FuelGauge {
+}
---
+++ ch6/car/Odometer.java
@@ -0,0 +1,10 @@
+/**
+ * Simulates a car's odometer.
+ *
+ * Responsibilities:
+ *   - Know/report the car's current mileage.
+ *   - Increment current mileage by 1 mile (Resets to 0 after 999,999).
+ *   - Decrease fuel in FuelGauge for every 24 miles traveled.
+ */
+public class Odometer {
+}
