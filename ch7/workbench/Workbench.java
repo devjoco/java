6890aaf871086b9d94c212a93acf5f6ff020cae3
devjoco <devonjoconnor@gmail.com>
2020-10-31 15:29:28 -0400

    Add Problem1, Problem2

--- ch7/workbench/Workbench.java
+++ ch7/workbench/Workbench.java
@@ -1,4 +1,18 @@
+/**
+ * Algorithm Workbench problems from ch7.
+ */
 public class Workbench {
     public static void main(String[] args) {
+        // Problem 1
+        int[] names = new int[20];
+        for(int name : names)
+            System.out.print(name + " ");
+        System.out.println();
+
+        // Problem 2
+        int[] numberArray1 = new int[100];
+        int[] numberArray2 = new int[100];
+        for(int i=0; i<numberArray1.length; i++)
+            numberArray2[i] = numberArray1[i];
     }
 }
