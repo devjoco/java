6365b69625cf024a511df0989f80e827239cdcb6
devjoco <devonjoconnor@gmail.com>
2020-10-31 15:33:37 -0400

    Add Problem3, rename P1 names to nums

--- ch7/workbench/Workbench.java
+++ ch7/workbench/Workbench.java
@@ -4,9 +4,9 @@
 public class Workbench {
     public static void main(String[] args) {
         // Problem 1
-        int[] names = new int[20];
-        for(int name : names)
-            System.out.print(name + " ");
+        int[] nums = new int[20];
+        for(int num : nums)
+            System.out.print(num + " ");
         System.out.println();
 
         // Problem 2
@@ -14,5 +14,11 @@ public class Workbench {
         int[] numberArray2 = new int[100];
         for(int i=0; i<numberArray1.length; i++)
             numberArray2[i] = numberArray1[i];
+
+        // Problem 3
+        String[] names = {"Einstein", "Newton", "Copernicus", "Kepler"};
+        for(String name : names)
+            System.out.print(name + " " + name.length() + ",");
+        System.out.println();
     }
 }
