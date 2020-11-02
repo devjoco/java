c402aaab41b59548c6775d57423eb9a80852f3e2
devjoco <devonjoconnor@gmail.com>
2020-11-01 23:05:51 -0500

    Add {g,s}etters, calc/display wages

--- ch7/payroll/Payroll.java
+++ ch7/payroll/Payroll.java
@@ -9,33 +9,48 @@ import java.text.DecimalFormat;
 public class Payroll {
     private int[] employeeId = {5658845, 4520125, 7895122, 
                        8777541, 8451277, 1302850, 7580489};
-    private int[] hours;
-    private double[] payRate, 
-                     wages;
+    private int[] hours      = new int[employeeId.length];
+    private double[] payRate = new double[employeeId.length],
+                     wages   = new double[employeeId.length];
+
+    public int[] getIds() { return employeeId; };
+    public double getWages(int idx) { return wages[idx]; }
+
+    public void setHours(int idx, int hours) {
+        this.hours[idx] = hours;
+    }
+
+    public void setRate(int idx, double payRate) {
+        this.payRate[idx] = payRate;
+    }
+
+    public void calcWages(int idx) {
+        wages[idx] = hours[idx] * payRate[idx];
+    }
 
     
     public static void main(String[] args) {
         Scanner       scan  = new Scanner(System.in);
         DecimalFormat money = new DecimalFormat("¤#,##0.00");
         Payroll       roll  = new Payroll();
+        int[]         ids   = roll.getIds();
 
         System.out.println("Enter each employee's hours and pay rate.");
-        ids = roll.getIds();                    // TODO
 
         for(int i=0; i<ids.length; i++) {
             System.out.printf("\nEmployee #%d\n", ids[i]);
             System.out.printf("Hours: ");
-            roll.setHours(i, scan.nextInt());   // TODO 
+            roll.setHours(i, scan.nextInt());
             System.out.printf("Rate: $");
-            roll.setRate(i, scan.nextInt());    // TODO 
-            roll.calcWages(i);                  // TODO
+            roll.setRate(i, scan.nextDouble());
+            roll.calcWages(i);
         }
 
         System.out.printf("┌───────────────────┐\n");
         System.out.printf("│ %-7s │ %-7s │\n", "Emp. ID", "Wages");
         for(int i=0; i<ids.length; i++)
             System.out.printf("│ %-7s │ %-7s │\n", 
-                    roll.getId(i), roll.getWages(i));
+                    ids[i], roll.getWages(i));
         System.out.printf("└───────────────────┘\n");
     }
 }
