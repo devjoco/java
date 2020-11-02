0ce568c9e58ab583b18b12d4b5a19a9938c498f1
devjoco <devonjoconnor@gmail.com>
2020-11-01 22:30:06 -0500

    Add Payroll class

---
+++ ch7/payroll/Payroll.java
@@ -0,0 +1,16 @@
+/**
+ * Payroll class.
+ * Asks user for each employee's hours and wages, calculates gross pay.
+ * Bounds checks to prevent negative numbers, payRate must be above $6.00.
+ */
+public class Payroll {
+    private int[] employeeId = {5658845, 4520125, 7895122, 
+                       8777541, 8451277, 1302850, 7580489};
+    private int[] hours;
+    private double[] payRate, 
+                     wages;
+
+    
+    public static void main(String[] args) {
+    }
+}
