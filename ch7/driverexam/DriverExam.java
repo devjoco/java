cf85a87d195cea495380c1626f76de88e6bdfaab
devjoco <devonjoconnor@gmail.com>
2020-11-03 18:20:34 -0500

    Add DriverExam class

---
+++ ch7/driverexam/DriverExam.java
@@ -0,0 +1,14 @@
+/**
+ * Simulates a driver's license exam.
+ *
+ * Holds the correct values for the 20 Qs.
+ * Holds the student's answers for the 20 Qs.
+ *
+ * Has methods to:
+ *   - Determine if student passed
+ *   - return the total number of correctly answered Qs
+ *   - return the total number of incorrect answers
+ *   - return the numbers of the Qs that were missed
+ */
+public class DriverExam {
+}
