1e3eab1a5cfe16837fdb5fbece396e823b2efe7d
devjoco <devonjoconnor@gmail.com>
2020-11-03 16:27:10 -0500

    Refactor -v checking, add -f checking

--- ch7/chargeAccount/DemoChargeAccount.java
+++ ch7/chargeAccount/DemoChargeAccount.java
@@ -1,9 +1,21 @@
 import java.util.Scanner;
+import java.io.*;
 
 public class DemoChargeAccount {
     public static void main(String[] args) { 
-        Scanner scan = new Scanner(System.in);
+        Scanner scan    = new Scanner(System.in);
         boolean verbose = false;
+        InputStream in  = System.in;
+
+        for(String arg: args) {
+            if (arg.equals("-v")) {
+                verbose = true;
+            }
+            if (arg.matches("-f=.*")) {
+                String filename = arg.substring(arg.indexOf("=")+1);
+                System.out.printf("Filename: %s\n", filename);
+            }
+        }
 
         if(args.length > 0 && args[0].equals("-v")) 
             verbose = true;
