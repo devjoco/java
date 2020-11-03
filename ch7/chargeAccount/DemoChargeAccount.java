19286347477530dd9b81a8dd012180757da8acd4
devjoco <devonjoconnor@gmail.com>
2020-11-03 12:17:50 -0500

    Print result from seq and bin searches

--- ch7/chargeAccount/DemoChargeAccount.java
+++ ch7/chargeAccount/DemoChargeAccount.java
@@ -14,10 +14,16 @@ public class DemoChargeAccount {
         System.out.print("Number: ");
         int num = scan.nextInt();
         while (num != -1) {
-            if(ChargeAccount.isValidBin(num, verbose))
-                System.out.println("Valid");
+            if(ChargeAccount.isValidSeq(num, verbose))
+                System.out.println("Sequential Valid");
             else
-                System.out.println("Invalid");
+                System.out.println("Sequential Invalid");
+
+            if(ChargeAccount.isValidBin(num, verbose))
+                System.out.println("Binary Valid");
+            else
+                System.out.println("Binary Invalid");
+
             System.out.println("\nWhat number are you checking? (-1 to exit)");
             System.out.print("Number: ");
             num = scan.nextInt();
