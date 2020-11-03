4c117144cd2df50debf720d7662ca0329253c100
devjoco <devonjoconnor@gmail.com>
2020-11-03 11:29:12 -0500

    Add demo for user to search numbers

--- ch7/chargeAccount/DemoChargeAccount.java
+++ ch7/chargeAccount/DemoChargeAccount.java
@@ -1,5 +1,21 @@
-public class DemoChargeAccount {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class DemoChargeAccount {
+    public static void main(String[] args) { 
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("What number are you looking for? (-1 to exit)");
+        System.out.print("Number: ");
+        int num = scan.nextInt();
+        while (num != -1) {
+            if(ChargeAccount.isValidSeq(num)) {
+                System.out.println("That's a valid number!");
+            } else {
+                System.out.println("Not a valid number!!!!");
+            }
+        System.out.println("What number are you looking for? (-1 to exit)");
+        System.out.print("Number: ");
+        num = scan.nextInt();
+        }
     }
 }
