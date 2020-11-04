68013652d32c15eb175d4c65204c8da5b2b331f7
devjoco <devonjoconnor@gmail.com>
2020-11-03 19:45:24 -0500

    Refine output

--- ch7/driverexam/DemoDriverExam.java
+++ ch7/driverexam/DemoDriverExam.java
@@ -11,25 +11,24 @@ public class DemoDriverExam {
             'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A'};
 
         DriverExam exam1 = new DriverExam(testPassAns);
-        System.out.println();
-        System.out.printf("Exam1 passed: %b, with %d correct, %d incorrect.\n",
-                exam1.passed(),
+        System.out.printf("Exam1 %s with %d correct, %d incorrect.\n",
+                exam1.passed()? "passed" : "didn't pass",
                 exam1.totalCorrect(),
                 exam1.totalIncorrect());
         exam1.printMissed();
 
         DriverExam exam2 = new DriverExam(testFailAns);
         System.out.println();
-        System.out.printf("Exam2 passed: %b, with %d correct, %d incorrect.\n",
-                exam2.passed(),
+        System.out.printf("Exam2 %s with %d correct, %d incorrect.\n",
+                exam2.passed()? "passed" : "didn't pass",
                 exam2.totalCorrect(),
                 exam2.totalIncorrect());
         exam2.printMissed();
 
         DriverExam exam3 = new DriverExam(testAllAAns);
         System.out.println();
-        System.out.printf("Exam3 passed: %b, with %d correct, %d incorrect.\n",
-                exam3.passed(),
+        System.out.printf("Exam3 %s with %d correct, %d incorrect.\n",
+                exam3.passed()? "passed" : "didn't pass",
                 exam3.totalCorrect(),
                 exam3.totalIncorrect());
         exam3.printMissed();
