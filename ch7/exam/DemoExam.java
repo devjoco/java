1a8ef94e5a00b570f269e5a7d20c0d3d9eb9dd4e
devjoco <devonjoconnor@gmail.com>
2020-11-06 01:06:42 -0500

    Remove hardcoded exam answers in Demo

--- ch7/exam/DemoExam.java
+++ ch7/exam/DemoExam.java
@@ -2,27 +2,8 @@ import java.io.*;
 
 public class DemoExam {
     public static void main(String[] args) throws IOException {
-        char[] answers1 = {
-            'B', 'A', 'B', 'D', 'C', 'C', 'B', 'A', 'B', 'A',
-            'C', 'C', 'A', 'A', 'A', 'B', 'A', 'A', 'A', 'A',
-            'A', 'B', 'A', 'A'},
-            answers2 = {
-            'A', 'B', 'C', 'D', 'A', 'B', 'C', 'D', 'A', 'B', 
-            'C', 'D', 'A', 'B', 'C', 'D', 'A', 'B', 'C', 'D',
-            'C', 'D', 'A', 'B'},
-            answers3 = {
-            'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 
-            'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A',
-            'A', 'A', 'A', 'A'};
-
-        Exam exam1 = new Exam(answers1, answers1);
-        Exam exam2 = new Exam(answers2, answers3);
-        Exam exam3 = new Exam(answers1, answers3);
-        Exam exam4 = new Exam("sampleTest.csv");
-        displayExamResults(exam1);
-        displayExamResults(exam2);
-        displayExamResults(exam3);
-        displayExamResults(exam4);
+        Exam exam = new Exam("sampleTest.csv");
+        displayExamResults(exam);
     }
 
     public static void displayExamResults(Exam exam) {
