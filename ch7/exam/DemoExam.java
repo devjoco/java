0f201702bd9544ee7ce023214f25e34b16239e43
devjoco <devonjoconnor@gmail.com>
2020-11-06 13:32:11 -0500

    Add ch6Exam.csv

--- ch7/exam/DemoExam.java
+++ ch7/exam/DemoExam.java
@@ -2,8 +2,11 @@ import java.io.*;
 
 public class DemoExam {
     public static void main(String[] args) throws IOException {
-        Exam exam = new Exam("sampleTest.csv");
-        displayExamResults(exam);
+        Exam ch6Exam = new Exam("ch6Exam.csv");
+        displayExamResults(ch6Exam);
+
+        Exam ch7Exam = new Exam("ch7Exam.csv");
+        displayExamResults(ch7Exam);
     }
 
     public static void displayExamResults(Exam exam) {
