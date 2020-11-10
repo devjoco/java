aaac8954cca85ebb4cb314053db20a098f3e9bc6
devjoco <devonjoconnor@gmail.com>
2020-11-09 22:02:27 -0500

    Add docstring for SalesStats

--- ch7/sales/SalesStats.java
+++ ch7/sales/SalesStats.java
@@ -1,5 +1,36 @@
 import java.util.Scanner;
 
+/**
+ * Quarterly Sales Statistics.
+ * Write a program that lets the user enter four quarterly sales figures for
+ * six divisions of a company. The figures should be stored in a 
+ * two-dimensional array. Once the figures are entered, the program should 
+ * display the following data for each quarter:
+ *   - A list of the sales figres by division
+ *   - Each division's increase or decrease from the previous quarter (not 1st)
+ *   - The total sales for the quarter
+ *   - The company's increase or decrease from the previous quarter (not 1st)
+ *   - The average sales for all divisions that quarter
+ *   - The division with the highest sales for that quarter
+ *
+ * Example:
+ * ┌─────┬─────┬────────────┬─────┬─────┬─────┬─────┬─────┬─────┐
+ * │     │   Q1│          Q2│   Q3│   Q4│Total│  Q2∆│  Q3∆│  Q4∆│
+ * ├─────┼─────┼────────────┼─────┼─────┼─────┼─────┼─────┼─────┤
+ * │D1   │ 3.00│        2.00│ 1.00│ 0.00│ 6.00│-1.00│-1.00│-1.00│
+ * │D2   │ 9.00│        1.00│ 3.00│ 2.00│15.00│-8.00│ 2.00│-1.00│
+ * │D3   │ 1.00│        1.00│ 2.00│ 1.00│ 5.00│ 0.00│ 1.00│-1.00│
+ * │D4   │ 1.00│        2.00│ 2.00│ 3.00│ 8.00│ 1.00│ 0.00│ 1.00│
+ * │D5   │ 3.00│        2.00│ 1.00│ 3.00│ 9.00│-1.00│-1.00│ 2.00│
+ * │D6   │ 0.00│        2.00│ 1.00│ 4.00│ 7.00│ 2.00│ 2.00│ 3.00│
+ * ├─────┼─────┼────────────┼─────┼─────┼─────┼─────┼─────┼─────┤
+ * │Total│17.00│       10.00│10.00│13.00│50.00│-7.00│-7.00│ 3.00│
+ * ├─────┼─────┼────────────┼─────┼─────┼─────┴─────┴─────┴─────┤
+ * │Avg  │ 2.83│        1.67│ 1.67│ 2.17│                       │
+ * ├─────┼─────┼────────────┼─────┼─────┼───────────────────────┤
+ * │Most │   D2│D1,2,3,4,5,6│   D2│   D4│                       │
+ * └─────┴─────┴────────────┴─────┴─────┴───────────────────────┘
+ */
 public class SalesStats { 
     public static void main(String[] args) { 
         Scanner scan = new Scanner(System.in);
