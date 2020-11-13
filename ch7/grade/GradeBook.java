15824f26b2d3d078acbe7c173a8c0e20a61ecb99
devjoco <devonjoconnor@gmail.com>
2020-11-13 18:05:16 -0500

    Add GradeBook class with prog desc

---
+++ ch7/grade/GradeBook.java
@@ -0,0 +1,29 @@
+/**
+ * Grade Book -- Programming Challenge 7, Page 506.
+ * A Teacher has five students who have taken four tests. The teacher uses the
+ * following grading scale to assign a letter grade to a student, based on the
+ * average of his or her four test scores.
+ *
+ *      Test Score  Letter Grade
+ *      90-100          A
+ *      80-89           B
+ *      70-79           C
+ *      60-69           D
+ *      0-59            F
+ *
+ * Write a class that uses a String array (or an ArrayList object) to hold the
+ * five students' names, an array of five characters to hold the five students'
+ * letter grades, and five arrays of four doubles each to hold each student's
+ * set of test scores. The class shoul dhave methods that return a specific 
+ * student's name, average test score, and a letter grade based on the average.
+ *
+ * Demonstrate the class in a program that allows the user to enter each
+ * student's name and his or her four test scores. It should then display each
+ * student's average test score and letter grade.
+ * Input validation: Don't accept test scores less than 0 or greater than 100.
+ *
+ * Modify the grade book application so it drops each student's lowest score
+ * when determining the test score averages and letter grades.
+ */
+public class GradeBook {
+}
