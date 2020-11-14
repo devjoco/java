68881a6fb6112f6dc5f9b5c636d0834731c62c00
devjoco <devonjoconnor@gmail.com>
2020-11-14 10:32:38 -0500

    Add Lottery class with prog desc

---
+++ ch7/lottery/Lottery.java
@@ -0,0 +1,27 @@
+/**
+ * Lottery Application ─ Programming Challenge 9, page 506.
+ * Write a Lottery class that simulates a lottery. The class should have an 
+ * array of five integers named lotteryNumbers. The constructor should 
+ * generate a random number in the range of 0 through 9 for each element in the
+ * array. Refer to Chapter 4's discussion of the Random class for generating 
+ * random numbers. The class should also have a method that accepts an array of
+ * five integers that represent a person's lottery picks. The method is to 
+ * compare the corresponding elements i nthe two arrays and return the number 
+ * of digits that match. For example, the following shows the lotteryNumbers 
+ * array and the user's array with sample numbers stored in each. There are two
+ * matching digits (elements 2 and 4).
+ *
+ *      lotteryNumbers array:  7 4 9 1 3
+ *              User's array:  4 2 9 7 3
+ *                       pos:  0 1 2 3 4 
+ *
+ * In addition, the class should have a method that returns a copy of the 
+ * lotteryNumbers array.
+ *
+ * Demonstrate the class in a program that asks the user to enter five numbers.
+ * The program should display the number of digits that match the randomly 
+ * generated lottery numbers. If all o fthe digits match, display a message 
+ * proclaiming the user a grand prize winner.
+ */
+public class Lottery {
+}
