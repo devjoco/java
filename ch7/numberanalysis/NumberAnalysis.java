81ed7b452eb4ab8e2f5054a91e7b251060907743
devjoco <devonjoconnor@gmail.com>
2020-11-14 13:52:43 -0500

    Add NumberAnalysis w/ prog desc

---
+++ ch7/numberanalysis/NumberAnalysis.java
@@ -0,0 +1,14 @@
+/**
+ * Number Analysis Class ─ Programming Challenge #11, Page 507.
+ *
+ * Write a class with a constructor that accepts a file name as its argument. 
+ * Asssume the file contains a series of numbers, each written on a separate 
+ * line. The class should read the contents of the file into an array, and 
+ * then display the following data:
+ *  - The lowest number in the array
+ *  - The highest number in the array
+ *  - The total of the number sin the array
+ *  - The average of the numbers in the array
+ */
+public class NumberAnalysis {
+}
