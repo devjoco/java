c9e0659371260838286be5e6e46b29441d365ada
devjoco <devonjoconnor@gmail.com>
2020-11-15 10:15:23 -0500

    Fix typo in Demo

--- ch7/numberanalysis/DemoAnalysis.java
+++ ch7/numberanalysis/DemoAnalysis.java
@@ -1,6 +1,8 @@
+import java.io.*;
+
 public class DemoAnalysis {
-    public static void main(String[] args) {
-        NumberAnalysis a = NumberAnalysis("numbers.txt");
+    public static void main(String[] args) throws IOException {
+        NumberAnalysis a = new NumberAnalysis("numbers.txt");
         System.out.println(a.getLowest());
         System.out.println(a.getHighest());
         System.out.println(a.getTotal());
