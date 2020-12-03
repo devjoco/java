1d81571d89853044d7055c48f36af510b193d79a
devjoco <devonjoconnor@gmail.com>
2020-12-03 17:59:50 -0500

    Add Person class definition

--- ch9/customer/Person.java
+++ ch9/customer/Person.java
@@ -1,2 +1,22 @@
 public class Person {
+    private String name;
+    private String address;
+    private String telephone;
+
+    /** Constructor */
+    public Person(String name, String address, String telephone) {
+        this.name      = name;
+        this.address   = address;
+        this.telephone = telephone;
+    }
+
+    /** Getters */
+    public String getName()      { return name;      }
+    public String getAddress()   { return address;   }
+    public String getTelephone() { return telephone; }
+
+    /** Setters */
+    public void setName(String name)           { this.name      = name;      }
+    public void setAddress(String address)     { this.address   = address;   }
+    public void setTelephone(String telephone) { this.telephone = telephone; }
 }
