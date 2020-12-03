06e7191faef2524b43454bb8f9b59380b6d090d1
devjoco <devonjoconnor@gmail.com>
2020-12-03 16:02:35 -0500

    Add FinalExam

---
+++ ch9/essay/FinalExam.java
@@ -0,0 +1,26 @@
+public class FinalExam extends GradedActivity {
+    private int    numQuestions;
+    private int    numMissed;
+    private double pointsEach;
+
+    /** Constructor */
+    public FinalExam(int questions, int missed) {
+        numQuestions = questions;
+        numMissed    = missed;
+        pointsEach   = 100.0 / numQuestions;
+    }
+
+    /** Getters */
+    public double getPointsEach() { return pointsEach; }
+    public int    getNumMissed()  { return numMissed;  }
+
+    @Override
+    public String toString() {
+        return super.toString()
+            + "\nFinalExam Details:"
+            + "\n\tQuestions:   "    + numQuestions
+            + "\n\tPoints Each: "    + pointsEach
+            + "\n\tMisses:      "    + numMissed
+    }
+
+}
