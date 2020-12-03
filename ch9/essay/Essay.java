d1e4cadaacbec2d16068fb66701fb6ad22afff95
devjoco <devonjoconnor@gmail.com>
2020-12-03 15:07:42 -0500

    Add constructor, getters, setters for essay

--- ch9/essay/Essay.java
+++ ch9/essay/Essay.java
@@ -6,4 +6,25 @@ public class Essay extends GradedActivity {
     private double grammarPts;  /* 30pts */
     private double lengthPts;   /* 20pts */
     private double spellingPts; /* 20pts */
+
+    /** Constructs an Essay with given pts for each section */
+    public Essay(double c, double g, double l, double s) {
+        contentPts  = c;
+        grammarPts  = g;
+        lengthPts   = l;
+        spellingPts = s;
+        setScore(c + g + l + s);
+    } 
+
+    /** Getters */
+    public double getContentPts()  { return contentPts;  }
+    public double getGrammarPts()  { return grammarPts;  }
+    public double getLengthPts()   { return lengthPts;   }
+    public double getSpellingPts() { return spellingPts; }
+
+    /** Setters */
+    public void setContentPts(double pts)  { contentPts  = pts; }
+    public void setGrammarPts(double pts)  { grammarPts  = pts; }
+    public void setLengthPts(double pts)   { lengthPts   = pts; }
+    public void setSpellingPts(double pts) { spellingPts = pts; }
 }
