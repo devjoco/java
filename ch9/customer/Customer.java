99cd5a2828963d4c9a390b8a5cbad91571cee1ae
devjoco <devonjoconnor@gmail.com>
2020-12-03 18:14:16 -0500

    Add Customer definition

--- ch9/customer/Customer.java
+++ ch9/customer/Customer.java
@@ -1,2 +1,27 @@
-public class Customer {
+public class Customer extends Person {
+    private static int numCustomers = 0;
+    private int number;
+    private boolean onMailingList;
+
+    /** Constructor */
+    public Customer(String name, String address, String telephone,
+            boolean onMailingList) {
+        super(name, address, telephone);
+        this.onMailingList = onMailingList;
+        setNumber();
+    }
+
+    /** Setters */
+    public void addToMailingList()      { onMailingList = true;  }
+    public void removeFromMailingList() { onMailingList = false; }
+
+    /** Assigns the customer a unique number. */
+    public void setNumber() { 
+        number = numCustomers;
+        numCustomers++;
+    }
+
+    /** Getters */
+    public int     getNumber()       { return number;        }
+    public boolean isOnMailingList() { return onMailingList; }
 }
