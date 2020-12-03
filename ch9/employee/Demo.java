79cb36fea128a508a0bfb1df732a54e2653f4519
devjoco <devonjoconnor@gmail.com>
2020-12-02 23:22:52 -0500

    Print ShiftSupervisors and TeamLeaders in Demo

--- ch9/employee/Demo.java
+++ ch9/employee/Demo.java
@@ -6,6 +6,7 @@ public class Demo {
                           "Harry", "Pam", "Liz", "Kelly", "Fred"};
 
         // Create Employees and print them out.
+        System.out.println("Printing Employee[Employee]");
         Employee[] emps = new Employee[names.length];
         for(int i=0; i<names.length; i++)
             emps[i] = new Employee(names[i]);
@@ -14,6 +15,7 @@ public class Demo {
         System.out.println();
 
         // Create ProductionWorkers and print them out.
+        System.out.println("Printing ProductionWorker[ProductionWorker]");
         ProductionWorker[] workers = new ProductionWorker[names.length];
         for(int i=0; i<names.length; i++)
             workers[i] = new ProductionWorker(names[i]);
@@ -22,11 +24,30 @@ public class Demo {
         System.out.println();
 
         // Create polymorphic ProductionWorkers and print them out.
+        System.out.println("Printing Employee[ProductionWorker]");
         Employee[] empWorkers = new Employee[names.length];
         for(int i=0; i<names.length; i++)
             empWorkers[i] = new ProductionWorker(names[i]);
         for(int i=0; i<empWorkers.length; i++)
             System.out.println(empWorkers[i]); 
+        System.out.println();
+
+        // Create ShiftSupervisors and print them out.
+        System.out.println("Printing ShiftSupervisor[ShiftSupervisor]");
+        ShiftSupervisor[] sups = new ShiftSupervisor[names.length];
+        for(int i=0; i<names.length; i++)
+            sups[i] = new ShiftSupervisor(names[i]);
+        for(int i=0; i<sups.length; i++)
+            System.out.println(sups[i]); 
+        System.out.println();
+
+        // Create TeamLeaders and print them out.
+        System.out.println("Printing TeamLeader[TeamLeader]");
+        TeamLeader[] leaders = new TeamLeader[names.length];
+        for(int i=0; i<names.length; i++)
+            leaders[i] = new TeamLeader(names[i]);
+        for(int i=0; i<leaders.length; i++)
+            System.out.println(leaders[i]); 
         System.out.println();
     }
 }
