6c455523582c72c28222b4b7b5bcaedbbb39dfca
devjoco <devonjoconnor@gmail.com>
2020-12-03 14:57:12 -0500

    Add GradedActivity, Essay w/ fields only

---
+++ ch9/essay/Essay.java
@@ -0,0 +1,9 @@
+/**
+ * Determines the grade a student receives on an essay.
+ */
+public class Essay extends GradedActivity { 
+    private double contentPts;  /* 30pts */
+    private double grammarPts;  /* 30pts */
+    private double lengthPts;   /* 20pts */
+    private double spellingPts; /* 20pts */
+}
---
+++ ch9/essay/GradedActivity.java
@@ -0,0 +1,21 @@
+public class GradedActivity {
+    private double score;
+
+    /** Set the GA's score to the given grade.  */
+    public void setScore(double s) {
+        score = s;
+    }
+
+    /** Return the GA's score.  */
+    public double getScore() {
+        return score;
+    }
+
+    /** Convert the GA's numeric score into a letter grade.  */
+    public char getGrade() {
+        return score >= 90? 'A':
+               score >= 80? 'B':
+               score >= 70? 'C':
+               score >= 60? 'D': 'F';
+    }
+}
