22b80a07509ace7c42cf795c21f4e9000e770748
devjoco <devonjoconnor@gmail.com>
2020-12-03 15:29:44 -0500

    Add Demo

---
+++ ch9/essay/Demo.java
@@ -0,0 +1,8 @@
+public class Demo {
+    public static void main(String[] args) {
+        Essay myEssay = new Essay(23, 29, 15, 10);
+        System.out.println(myEssay);
+        Essay yourEssay = new Essay(30, 30, 20, 20);
+        System.out.println(yourEssay);
+    }
+}
