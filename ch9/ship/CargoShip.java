74ea35686eb5328a2481403cd8eb67ba8c4cd7b8
devjoco <devonjoconnor@gmail.com>
2020-12-03 21:28:59 -0500

    Add {Cargo,Cruise}Ship definitions

--- ch9/ship/CargoShip.java
+++ ch9/ship/CargoShip.java
@@ -1,1 +1,22 @@
-public class CargoShip extends Ship {}
+public class CargoShip extends Ship {
+    private int capacity;
+
+    /* Constructor */
+    public CargoShip(String name, String year, int capacity) {
+        super(name, year);
+        this.capacity = capacity;
+    }
+
+    /* Getters */
+    public int getCapacity() { return capacity; }
+
+    /* Setters */
+    public void setCapacity(int newCap) { capacity = newCap; }
+
+    @Override
+    public String toString() {
+        return super.toString()
+            + "\nCargoShip"
+            + "\n\tCapacity: " + capacity;
+    }
+}
--- ch9/ship/CruiseShip.java
+++ ch9/ship/CruiseShip.java
@@ -1,1 +1,22 @@
-public class CruiseShip extends Ship {}
+public class CruiseShip extends Ship {
+    private int maxPassengers;
+
+    /* Constructor */
+    public CruiseShip(String name, String year, int maxPassengers) {
+        super(name, year);
+        this.maxPassengers = maxPassengers;
+    }
+
+    /* Getters */
+    public int getMaxPassengers() { return maxPassengers; }
+
+    /* Setters */
+    public void setMAxPassengers(int newMax) { maxPassengers = newMax; }
+
+    @Override
+    public String toString() {
+        return super.toString() 
+            + "\nCruiseShip" 
+            + "\n\tMax Passengers: " + maxPassengers;
+    }
+}
