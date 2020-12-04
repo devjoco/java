b10e2b897369edad1710e9d3464a1a83f52fd1f2
devjoco <devonjoconnor@gmail.com>
2020-12-03 21:10:19 -0500

    Add {Bank,Savings}Account definitions

--- ch9/account/BankAccount.java
+++ ch9/account/BankAccount.java
@@ -1,2 +1,50 @@
 public abstract class BankAccount {
+    private double balance;
+    private double interestRate;
+    private double monthlyCharges;
+    private int    monthlyDeposits;
+    private int    monthlyWithdrawls;
+
+    /** Constructor */
+    public BankAccount(double balance, double interestRate) {
+        this.balance           = balance;
+        this.interestRate      = interestRate;
+        this.monthlyCharges    = 0.00;
+        this.monthlyDeposits   = 0;
+        this.monthlyWithdrawls = 0;
+    }
+
+    /** Getters */
+    public double getBalance()    { return balance;           }
+    public int    getWithdrawls() { return monthlyWithdrawls; }
+
+    /** Add a monthly charge */
+    public void addMonthlyCharge(double amt) {
+        monthlyCharges += amt;
+    }
+
+    /** Deposits given amount, increments deposits count. */
+    public void deposit(double amt) { 
+        balance += amt; 
+        monthlyDeposits++;
+    }
+
+    /** Withdraws given amount, increments withdrawls count. */
+    public void withdraw(double amt) {
+        balance -= amt;
+        monthlyWithdrawls++;
+    }
+
+    /** Adds monthly interest to balance. */
+    public void calcInterest() {
+        balance *= 1 + interestRate / 12;
+    }
+
+    /** Called once a month. */
+    public void monthlyProcess() {
+        balance -= monthlyCharges;
+        calcInterest();
+        monthlyDeposits   = 0;
+        monthlyWithdrawls = 0;
+    }
 }
--- ch9/account/SavingsAccount.java
+++ ch9/account/SavingsAccount.java
@@ -1,2 +1,41 @@
-public class SavingsAccount {
+public class SavingsAccount extends BankAccount {
+    private final double MIN_BALANCE    = 25.00;
+    private final double MAX_WITHDRAWLS = 4;
+    private boolean accountActive; // True if blance >= $25
+
+    /** Determine whether account should be active or inactive */
+    private void updateStatus() {
+        if(getBalance() < MIN_BALANCE) accountActive = false;
+        else                           accountActive = true;
+    }
+
+    /** Constructor */
+    public SavingsAccount(double balance, double interestRate) {
+        super(balance, interestRate);
+        updateStatus();
+    }
+
+
+    /** Determine if active before withdrawl, check if still active after. */
+    public void withdraw(double amt) {
+        if(accountActive) {
+            super.withdraw(amt);
+            updateStatus();
+        }
+    }
+
+    /** Deposit amount, then check to see if account is active. */
+    public void deposit(double amt) {
+        super.deposit(amt);
+        updateStatus();
+    }
+
+    /** Add fee for exceeding alotted withdrawls. */
+    public void monthlyProcess() {
+        int withdrawls = getWithdrawls();
+        if(withdrawls > MAX_WITHDRAWLS)
+            addMonthlyCharge(withdrawls - MAX_WITHDRAWLS);
+        super.monthlyProcess();
+        updateStatus();
+    }
 }
