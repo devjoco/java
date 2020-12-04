e68524f4500bde8b469feefe63729abb0fa08179
devjoco <devonjoconnor@gmail.com>
2020-12-03 21:21:05 -0500

    Add Ship definition

--- ch9/ship/Ship.java
+++ ch9/ship/Ship.java
@@ -1,1 +1,26 @@
-public class Ship {}
+public class Ship {
+    private String name;
+    private String year;
+
+    /* Constructor */
+    public Ship(String name, String year) {
+        this.name = name;
+        this.year = year;
+    }
+
+    /* Getters */
+    public String getName() { return name; } 
+    public String getYear() { return year; }
+    
+    /* Setters */
+    public void setName(String name) { this.name = name; } 
+    public void setYear(String year) { this.year = year; }
+
+    @Override
+    public String toString() {
+        return super.toString()
+            + "\nShip"
+            + "\n\tName: " + name
+            + "\n\tYear: " + year;
+    }
+}
