09b2df5179b48e1da59585cd61da38257204fc83
devjoco <devonjoconnor@gmail.com>
2020-12-03 19:06:43 -0500

    Print out each instance

--- ch9/customer/Demo.java
+++ ch9/customer/Demo.java
@@ -2,13 +2,19 @@ public class Demo {
     public static void main(String[] args) {
         /* Test Person Class */
         Person myP = new Person("Devon", "17 University Ct.", "(862)216-9965");
+        System.out.println(myP);
+        System.out.println();
 
         /* Test Customer Class */
         Customer myC = new Customer("Devon", "17 University Ct.", 
                 "(862)216-9965", false);
+        System.out.println(myC);
+        System.out.println();
 
         /* Test PreferredCustomer Class */
         PreferredCustomer myPC = new PreferredCustomer("Devon", 
                 "17 University Ct.", "(862)216-9965", false, 1292.53);
+        System.out.println(myPC);
+        System.out.println();
     }
 }
