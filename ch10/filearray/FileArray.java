66ac2156e882cc133dff6855eeaedb4897d87c1c
devjoco <devonjoconnor@gmail.com>
2020-12-08 12:44:17 -0500

    Add Demo, increment pos in readFile()

--- ch10/filearray/FileArray.java
+++ ch10/filearray/FileArray.java
@@ -23,6 +23,7 @@ public class FileArray {
         while(!EOFReached) {
             try {
                 arrayRef[pos] = file.readInt();
+                pos++;
             } catch(EOFException e) {
                 EOFReached = true;
             }
@@ -31,6 +32,43 @@ public class FileArray {
     }
 
     /* Demostrate writeArray and readArray in program. */
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+            throws IOException 
+    {
+        int[] intsToWrite = {1, 2, 3, 4, 5, 6, 7, 8};
+        int[] intsToRead  = new int[intsToWrite.length];
+
+        /* Print out starting values for intsToWrite */
+        System.out.print("intsToWrite: ");
+        for(int i: intsToWrite)
+            System.out.print(i + " ");
+        System.out.print("\n");
+
+        /* Print out starting values for intsToRead */
+        System.out.print("intsToRead: ");
+        for(int i: intsToRead)
+            System.out.print(i + " ");
+        System.out.print("\n");
+
+        /* Write intsToWrite to myInts.bin */
+        System.out.println("Writing intsToWrite to myInts.bin");
+        FileArray.writeArray("myInts.bin", intsToWrite);
+
+        /* Read myInts.bin into intsToRead */
+        System.out.println("Reading myInts.bin into intsToRead");
+        FileArray.readArray("myInts.bin", intsToRead);
+
+        /* Print out final values for intsToWrite */
+        System.out.print("intsToWrite: ");
+        for(int i: intsToWrite)
+            System.out.print(i + " ");
+        System.out.print("\n");
+
+        /* Print out final values for intsToRead */
+        System.out.print("intsToRead: ");
+        for(int i: intsToRead)
+            System.out.print(i + " ");
+        System.out.print("\n");
+
     }
 }
