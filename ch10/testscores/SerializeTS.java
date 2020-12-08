397b06a42dc806cb71ef25c54d6070d42e36d999
devjoco <devonjoconnor@gmail.com>
2020-12-08 17:21:07 -0500

    Transfer TSs from TS1 to TS2 via serialized file

--- ch10/testscores/SerializeTS.java
+++ ch10/testscores/SerializeTS.java
@@ -2,7 +2,8 @@ import java.util.Random;
 import java.io.*;
 
 public class SerializeTS {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) 
+            throws IOException, ClassNotFoundException {
         final String filename = "tsArrays.bin";
 
         /* Sample data to create TestScores from */
@@ -17,13 +18,13 @@ public class SerializeTS {
         double[] ts9 = {12, 23, 34, 45, 56, 67, 78, 89, 90};
         double[][] ts = {ts1, ts2, ts3, ts4, ts5, ts6, ts7, ts8, ts9};
 
-        /* Create an un-init array of TestScores */
-        TestScores[] TS1 = new TestScores[ts.length];
-
         /* Create an array of TestScores from sample data */
-        TestScores[] TS2 = new TestScores[ts.length];
+        TestScores[] TS1 = new TestScores[ts.length];
         for(int i=0; i<ts.length; i++)
-            TS2[i] = new TestScores(ts[i]);
+            TS1[i] = new TestScores(ts[i]);
+
+        /* Create an un-init array of TestScores */
+        TestScores[] TS2 = new TestScores[ts.length];
 
         /* Print out both arrays of TestScores */
         System.out.println("TS1:");
@@ -33,15 +34,36 @@ public class SerializeTS {
         printTSArray(TS2);
 
         /* Serialize array of TestScores to a file */
-        DataOutputStream file = new DataOutputStream(
+        ObjectOutputStream outFile = new ObjectOutputStream(
                 new FileOutputStream(filename));
+        for(TestScores testScore: TS1)
+            outFile.writeObject(testScore);
+        outFile.close();
 
         /* Deserialize TestScores from file into empty array */
+        boolean EOFReached = false;
+        int pos = 0;
+        ObjectInputStream inFile = new ObjectInputStream(
+                new FileInputStream(filename));
+        while(!EOFReached) {
+            try {
+                TS2[pos] = (TestScores) inFile.readObject();
+                pos++;
+            } 
+            catch(EOFException e) {
+                EOFReached = true;
+            }
+        }
+        inFile.close();
 
         /* Print out both arrays of TestScores */
-
-        /* Close file */
-        file.close();
+        System.out.println();
+        System.out.println();
+        System.out.println("TS1:");
+        printTSArray(TS1);
+        System.out.println();
+        System.out.println("TS2:");
+        printTSArray(TS2);
     }
 
     public static void printTSArray(TestScores[] ts) {
