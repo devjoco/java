a5148626e332039085715f273ecaa4218bd82990
devjoco <devonjoconnor@gmail.com>
2020-12-08 12:15:56 -0500

    Add body to writeArray()

--- ch10/filearray/FileArray.java
+++ ch10/filearray/FileArray.java
@@ -1,6 +1,15 @@
+import java.io.*;
+
 public class FileArray {
     /* Open file as binary file, write contents of array, close file. */
-    public static void writeArray(String filename, int[] arrayRef) {
+    public static void writeArray(String filename, int[] arrayRef) 
+        throws IOException 
+    {
+        DataOutputStream file = new DataOutputStream(
+                new FileOutputStream(filename));
+        for(int i: arrayRef)
+            file.writeInt(i);
+        file.close();
     }
 
     /* Open file as binary file, read contents into array, close file. */
