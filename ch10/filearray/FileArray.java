ca03b0924263c988a5fcabb972fd05e76e65fa87
devjoco <devonjoconnor@gmail.com>
2020-12-08 12:11:08 -0500

    Add skeleton for FileArray

---
+++ ch10/filearray/FileArray.java
@@ -0,0 +1,13 @@
+public class FileArray {
+    /* Open file as binary file, write contents of array, close file. */
+    public static void writeArray(String filename, int[] arrayRef) {
+    }
+
+    /* Open file as binary file, read contents into array, close file. */
+    public static void readArray(String filename, int[] arrayRef) {
+    }
+
+    /* Demostrate writeArray and readArray in program. */
+    public static void main(String[] args) {
+    }
+}
