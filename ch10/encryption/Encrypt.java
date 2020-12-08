ed012a9c47c246d62566ecb3aa66ca8b21ef5fca
devjoco <devonjoconnor@gmail.com>
2020-12-08 13:40:39 -0500

    Open inFile, open outFile, close both

--- ch10/encryption/Encrypt.java
+++ ch10/encryption/Encrypt.java
@@ -1,12 +1,37 @@
+import java.util.Scanner;
+import java.io.*;
+
 public class Encrypt {
     public static void usage() {
         System.out.println("Usage: java Encrypt <filename> <password>");
         System.exit(0);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        /* Make sure a filename is given and a password is given. */
         boolean twoArgs = args.length == 2;
         if(!twoArgs)
             Encrypt.usage();
+
+        Scanner     inFile   = null;
+        PrintWriter outFile  = null;
+        String      filename = args[0];
+        String      password = args[1];
+
+        /* Open the given file for reading. */
+        try {
+            inFile = new Scanner(new File(filename));
+        } catch(FileNotFoundException e) {
+            System.out.println("That file doesn't exist.");
+            System.exit(0);
+        }
+
+        /* Open another file for writing. */
+        outFile = new PrintWriter(new File(filename + ".enc"));
+
+        /* Read each character into second file offset according to pw. */
+        /* Close the files. */
+        inFile.close();
+        outFile.close();
     }
 }
